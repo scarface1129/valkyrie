57cd56bdccc0f60af2b677cfce366690fb48f445
Scarface <agboemmanuel002@gmail.com>
2022-05-19 13:10:23 +0100

    settings.py

--- valkyrie/settings.py
+++ valkyrie/settings.py
@@ -132,7 +132,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 # STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # STATIC_ROOT = BASE_DIR / 'staticfiles'
@@ -142,6 +141,10 @@ MEDIA_URL = '/media/'
 
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_ROOT, 'static'),
+]
 MEDIAFILES_DIRS = [
     os.path.join(BASE_DIR, '/media/'),
 ]
