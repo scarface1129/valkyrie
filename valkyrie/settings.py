08a1c18919291573a80e5808e496dad58a58cb69
Scarface <agboemmanuel002@gmail.com>
2022-05-19 12:26:21 +0100

    added new static directory

--- valkyrie/settings.py
+++ valkyrie/settings.py
@@ -134,11 +134,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
+
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 MEDIAFILES_DIRS = [
     os.path.join(BASE_DIR, '/media/'),
 ]
