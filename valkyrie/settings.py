f67f8ad8a31ce157d98b1a162507e91dbb82b365
Scarface <agboemmanuel002@gmail.com>
2022-05-19 09:12:36 +0100

    settings

--- valkyrie/settings.py
+++ valkyrie/settings.py
@@ -133,6 +133,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, '/static/'),
 ]
