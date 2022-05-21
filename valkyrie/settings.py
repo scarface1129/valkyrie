6fdfe5c6a650c2b3d984fe439a0b82cf616afca7
Scarface <agboemmanuel002@gmail.com>
2022-05-21 22:44:03 +0100

    settings

--- valkyrie/settings.py
+++ valkyrie/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'valkyrie.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [str(BASE_DIR.joinpath('templates'))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
