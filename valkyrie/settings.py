ae31c45073fd5defc298602374bef4e0a8518e75
Scarface <agboemmanuel002@gmail.com>
2022-05-21 23:35:45 +0100

    settings

--- valkyrie/settings.py
+++ valkyrie/settings.py
@@ -15,9 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-_DIR = Path(__file__).resolve().parent.parent 
-print(_DIR)
-print(_DIR / 'Templates')
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -63,7 +61,7 @@ ROOT_URLCONF = 'valkyrie.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [_DIR / 'templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'Templates'), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,11 +73,6 @@ TEMPLATES = [
         },
     },
 ]
-
-# DIRNAME = os.path.abspath(os.path.dirname(__file__))
-# TEMPLATE_DIRS = (
-#     os.path.join(DIRNAME, 'Templates/'),
-# )
 
 WSGI_APPLICATION = 'valkyrie.wsgi.application'
 AUTH_USER_MODEL = "Users.User"
